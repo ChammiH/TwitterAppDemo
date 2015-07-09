0423a89fa804ca48de0a4ed79a6f89ebad601829
ChammiH <ChandimaHerath@Chandimas-iMac.local>
2015-07-09 13:15:36 +1000

    Initial Commit

---
+++ TwitterAppDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwitterAppDemo
+//
+//  Created by Chandima Herath on 9/07/15.
+//  Copyright (c) 2015 NukaWare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwitterAppDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwitterAppDemo
+//
+//  Created by Chandima Herath on 9/07/15.
+//  Copyright (c) 2015 NukaWare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
