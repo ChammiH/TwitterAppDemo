f28c184c3c891f293e88f812c77b2f407de28697
ChammiH <ChandimaHerath@Chandimas-iMac.local>
2015-07-09 14:43:01 +1000

    All the basic Twitter View function are working now.

--- TwitterAppDemo/TwitterViewController.h
+++ TwitterAppDemo/TwitterViewController.h
@@ -9,11 +9,22 @@
 #import <UIKit/UIKit.h>
 @class TweetObject;
 
+typedef NS_ENUM(NSInteger, TwitterViewStatus) {
+    TVDefault,
+    TVLoadedTweets,
+    TVLoadingTweetsFailed
+};
+
 @interface TwitterViewController : UIViewController
 
+-(void)loadTweets;
 -(IBAction)swipedRight:(UISwipeGestureRecognizer *)sender;
 -(IBAction)swipedLeft:(UISwipeGestureRecognizer *)sender;
 -(IBAction)swipedDown:(UISwipeGestureRecognizer *)sender;
+
+@property (readonly, nonatomic) TwitterViewStatus tvStatus;
+@property (readonly, strong, nonatomic) NSString *errorMessage;
+@property (nonatomic, strong) NSString *screenName;
 
 @end
 
--- TwitterAppDemo/TwitterViewController.m
+++ TwitterAppDemo/TwitterViewController.m
@@ -7,9 +7,14 @@
 //
 
 #import "TwitterViewController.h"
+#import "TwitterManager.h"
+#import "TweetBuilder.h"
+#import "TweetObject.h"
 
-@interface TwitterViewController () {
-    
+@interface TwitterViewController ()<TwitterBuilderProtocol> {
+    BOOL swipingTweet;
+    TweetObject *currentTweet;
+    UILabel *displayLabel;
 }
 @end
 
@@ -19,6 +24,34 @@
     [super viewDidLoad];
 }
 
+
+-(void)loadTweets
+{
+    _tvStatus = TVDefault;
+    _errorMessage = @"";
+    swipingTweet = true;
+    
+    [[TweetBuilder sharedObject] loadTweetsWithNewScreenName:_screenName Delegate:self];
+}
+
+
+-(void)loadingTweetsComplete
+{
+    currentTweet = [[TweetBuilder sharedObject] getUserInfoTweet];
+    [self.view addSubview:currentTweet];
+    swipingTweet = false;
+    
+    _tvStatus = TVLoadedTweets;
+}
+
+
+-(void)loadingTweetsFailed:(NSError *)error
+{
+    _errorMessage = [error.userInfo valueForKey:@"error_info"];
+    _tvStatus = TVLoadingTweetsFailed;
+}
+
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     
@@ -27,17 +60,62 @@
 
 - (IBAction)swipedDown:(UISwipeGestureRecognizer *)sender
 {
-    
+    [TweetObject resetScreenName];
+    [[TweetBuilder sharedObject] resetScreenName];
+    [[TwitterManager sharedObject] resetScreenName];
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 
 - (IBAction)swipedLeft:(UISwipeGestureRecognizer *)sender {
     
+    if (!swipingTweet) {
+        TweetObject *newTweet;
+        
+        if([[TweetBuilder sharedObject] hasANextTweet]) {
+            newTweet = [[TweetBuilder sharedObject] getNextTweet];
+        }else {
+            newTweet = nil;
+        }
+        
+        if(newTweet != nil) {
+            
+            UIView *oldTweet = currentTweet;
+            currentTweet = newTweet;
+            [self.view addSubview:currentTweet];
+            swipingTweet = true;
+            
+            [UIView animateWithDuration:0.5 delay:0 options: UIViewAnimationOptionCurveEaseOut animations:^{ [oldTweet setFrame:[currentTweet getOffsetLeftPosition]]; } completion:^(BOOL finished){ swipingTweet = false; [oldTweet removeFromSuperview]; }];
+            
+            [UIView animateWithDuration:0.5 delay:0 options: UIViewAnimationOptionCurveEaseOut animations:^{ [currentTweet setFrame:[currentTweet getOffsetCenterPosition]]; } completion:nil];
+        }
+    }
 }
 
 
 -(IBAction)swipedRight:(UISwipeGestureRecognizer *)sender {
     
+    if (!swipingTweet) {
+        TweetObject *newTweet;
+        
+        if([[TweetBuilder sharedObject] hasAPreviousTweet]) {
+            newTweet = [[TweetBuilder sharedObject] getPreviousTweet];
+        }else {
+            newTweet = nil;
+        }
+        
+        if (newTweet != nil) {
+            
+            UIView *oldTweet = currentTweet;
+            currentTweet = newTweet;
+            [self.view addSubview:currentTweet];
+            swipingTweet = true;
+            
+            [UIView animateWithDuration:0.5 delay:0 options: UIViewAnimationOptionCurveEaseOut animations:^{ [oldTweet setFrame:[currentTweet getOffsetRightPosition]]; } completion:^(BOOL finished){ swipingTweet = false; [oldTweet removeFromSuperview]; }];
+            
+            [UIView animateWithDuration:0.5 delay:0 options: UIViewAnimationOptionCurveEaseOut animations:^{ [currentTweet setFrame:[currentTweet getOffsetCenterPosition]]; } completion:nil];
+        }
+    }
 }
 
 @end
